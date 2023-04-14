791541f26a95c55334fda61e0409a07b1bca2954
liona <theajaslan@gmail.com>
2023-04-14 11:22:08 +0600

    changed

--- app/src/main/java/com/example/taskapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/dashboard/DashboardFragment.kt
@@ -17,8 +17,6 @@ class DashboardFragment : Fragment() {
     private var _binding: FragmentDashboardBinding? = null
     private lateinit var db: FirebaseFirestore
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
     private lateinit var pref: Pref
     override fun onCreateView(
